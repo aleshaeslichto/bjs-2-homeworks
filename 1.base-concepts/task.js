fecdfce9939de84bd53db03b0a9365e3f2271c82
aleshaeslichto <i@aleshaeslichto.ru>
2023-05-11 15:17:35 +0300

    add 1st ex.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+
+  const d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return "D = 0 => Корней нет.";
+  }
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else {
+    arr.push(-b / (2 * a));
+  }
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
