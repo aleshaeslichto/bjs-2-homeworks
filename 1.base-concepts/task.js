6730c821515281810c8d04f8bfb20ae2e35e8030
aleshaeslichto <i@aleshaeslichto.ru>
2023-05-15 11:19:14 +0300

    Fix homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,17 +5,36 @@ function solveEquation(a, b, c) {
 
   const d = Math.pow(b, 2) - 4 * a * c;
 
-  if (d < 0) {
-    return arr;
-  }
   if (d > 0) {
     arr.push((-b + Math.sqrt(d)) / (2 * a));
     arr.push((-b - Math.sqrt(d)) / (2 * a));
-  } else {
+  } else if (d == 0) {
     arr.push(-b / (2 * a));
   }
 
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+
+  // Проверка
+  if (isNaN(percent)) {
+    return `Параметр percent содержит неправильное значение ${percent}`;
+  }
+  if (isNaN(contribution)) {
+      return `Параметр contribution содержит неправильное значение ${contribution}`;
+  }
+  if (isNaN(amount)) {
+      return `Параметр amount содержит неправильное значение ${amount}`;
+  }
+  if (isNaN(countMonths)) {
+      return `Параметр CountMonths содержит неправильное значение ${countMonths}`;
+  }
+
+  // Вычисления
+  const refundAmount = amount - contribution; // тело кредита
+  let P = (percent / 100) / 12; // Новый формат процентов
+  const conditions = refundAmount * (P + (P / (((1 + P)**countMonths) - 1))); // Плата
+  let totalAmount = conditions * countMonths; // Финальная сумма
+  return parseFloat(totalAmount.toFixed(2)); // Парсим число с "плавающей" запятой
+  }
