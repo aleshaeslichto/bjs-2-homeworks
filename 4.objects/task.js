5fab4d2a59e536083aa609bb01c7703457005716
aleshaeslichto <i@aleshaeslichto.ru>
2023-06-19 11:35:10 +0300

    upload hw

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
+// 1
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+new Student("Максим", "мужской", 21)
+new Student("Инна", "женский", 22)
+
+// 2
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
+// 3
+Student.prototype.addMarks = function(...marks) {
+    if (this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+// 4
 Student.prototype.addMarks = function (...marks) {
-  
+  return this.marks ? this.marks.push(...marks)
 }
 
-Student.prototype.getAverage = function () {
-  
+// 5 ср. ар.
+Student.prototype.getAverage = function() {
+    if (this.marks === undefined) {
+        return null
+    } else {
+        return (this.marks.reduce((previous, next) => previous + next)) / this.marks.length
+    }
 }
 
+// 6
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks
+  this.excluded = reason;
 }
