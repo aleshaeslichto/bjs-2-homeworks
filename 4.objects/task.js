605b18f539691d19228fd1ba316046c97a28a68e
aleshaeslichto <i@aleshaeslichto.ru>
2023-06-21 13:38:35 +0300

    task.js updated

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,14 +10,6 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marks) {
-    if (this.marks === undefined) {
-        this.marks = [mark];
-    } else {
-        this.marks.push(mark);
-    }
-}
-
-Student.prototype.addMarks = function(...marks) {
     return this.marks ? this.marks.push(...marks) : "student is excluded"
 }
 
